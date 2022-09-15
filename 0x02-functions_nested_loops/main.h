21a9cf1f7cfe8a9e77b396d9e1a02592ddbdda0d
Ohbasi30 <ohbasi30@gmail.com>
2022-09-15 01:40:27 +0000

    created new files

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,17 +1,32 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN.H
+#define MAIN.H
 
 void print_alphabet(void);
+
 void print_alphabet_x10(void);
+
+void fibonacci(void);
+
+int _putchar(char c);
+
 int _islower(int c);
+
 int _isalpha(int c);
+
 int print_sign(int n);
+
 int _abs(int);
-int print_last_digit(int);
+
 void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
+
+int print_last_digit(int);
+
 void print_times_table(int n);
+
+void times_table(void);
+
+int add(int a, int b);
+
+void print_to_98(int n);
 
 #endif
