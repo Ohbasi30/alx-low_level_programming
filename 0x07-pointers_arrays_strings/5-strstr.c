944f613413fc4abcb56652df82383d36f262b15b
Ohbasi30 <Ohbasi30@gmail.com>
2022-09-28 10:01:20 +0100

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -5,6 +5,7 @@
  * _strstr - finds the first occurence of needle in haystack
  * @haystack: where we look
  * @needle: what we look for
+ *
  * 
  * Return: pointer to beginning of located substring or NULL
  */
@@ -24,7 +25,7 @@ char *_strstr(char *haystack, char *needle)
 			if (*(haystack + point) != needle[point])
 				break;
 		}
-		if (needle[point] == '\0')
+		        if (needle[point] == '\0')
 			return (haystack);
 	}
 	return (NULL);
